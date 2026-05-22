978bc67654e7e3dd59749fabd9be6e4b488583ed
Manuel <manuelalberto.gorrin@gmail.com>
2026-05-22 19:10:18 -0400

    add CRUD operations to PlayerController

--- src/player/player.controller.ts
+++ src/player/player.controller.ts
@@ -1,4 +1,29 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Get, Param } from '@nestjs/common';
 
 @Controller('player')
-export class PlayerController {}
+export class PlayerController {
+    @Get()
+    getAllPlayers(): string {
+        return "All Players";
+    }
+
+    @Get(":id")
+    getPlayerById(@Param("id") id: string): string {
+        return `Player ID: ${id}`;
+    }
+
+    @Get("create/:name")
+    createPlayer(@Param("name") name: string): string {
+        return `Player ${name} created`;
+    }
+
+    @Get("update/:id/:name")
+    updatePlayer(@Param("id") id: string, @Param("name") name: string): string {
+        return `Player ID: ${id} updated to ${name}`;
+    }
+
+    @Get("delete/:id")
+    deletePlayer(@Param("id") id: string): string {
+        return `Player ID: ${id} deleted`;
+    }
+}
