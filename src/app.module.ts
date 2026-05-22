0cc78281b5a642b4e2afd0d8f9bf8db723925c3a
Manuel <manuelalberto.gorrin@gmail.com>
2026-05-22 19:12:18 -0400

    add PlayerService to AppModule and create PlayerService class

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,10 +2,11 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { PlayerController } from './player/player.controller';
+import { PlayerService } from './player/player.service';
 
 @Module({
   imports: [],
   controllers: [AppController, PlayerController],
-  providers: [AppService],
+  providers: [AppService, PlayerService],
 })
 export class AppModule {}
