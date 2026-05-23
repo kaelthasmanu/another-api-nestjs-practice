9dea7c4243318fc1dd43c49e7f31d0789529ad30
Manuel <manuelalberto.gorrin@gmail.com>
2026-05-23 16:12:27 -0400

    feat: add Project entity and update ProjectsController to return project data

---
+++ src/projects/entities/project.entity.ts
@@ -0,0 +1,8 @@
+export class Project {
+  id: number;
+  key: string;
+  title: string;
+  description: string;
+  plannedHours: Date;
+  owner: any;
+}
--- src/projects/projects.controller.ts
+++ src/projects/projects.controller.ts
@@ -7,19 +7,39 @@ import {
   Patch,
   Post,
 } from '@nestjs/common';
+import { Project } from './entities/project.entity';
+
+const mock = [
+  {
+    id: 1,
+    key: 'project-1',
+    title: 'Project 1',
+    description: 'Description of project 1',
+    plannedHours: new Date(),
+    owner: { id: 1, name: 'Owner 1' },
+  },
+  {
+    id: 2,
+    key: 'project-2',
+    title: 'Project 2',
+    description: 'Description of project 2',
+    plannedHours: new Date(),
+    owner: { id: 2, name: 'Owner 2' },
+  },
+];
 
 @Controller('projects')
 export class ProjectsController {
   @HttpCode(HttpStatus.OK)
   @Get()
-  getManyProjects() {
-    return 'This action returns all projects';
+  getManyProjects(): Project[] {
+    return mock;
   }
 
   @HttpCode(HttpStatus.OK)
-  @Get(':projetcId')
-  getOneProject(): string {
-    return 'This action returns one project';
+  @Get(':projectId')
+  getOneProject(): Project {
+    return mock[0];
   }
   @HttpCode(HttpStatus.CREATED)
   @Post()
