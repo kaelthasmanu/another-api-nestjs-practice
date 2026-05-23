4086f7e586b511295b5fd6b83e972ec14739d281
Manuel <manuelalberto.gorrin@gmail.com>
2026-05-23 16:05:40 -0400

    refactor: organize imports and add HTTP status codes to project routes

--- src/projects/projects.controller.ts
+++ src/projects/projects.controller.ts
@@ -1,27 +1,37 @@
-import { Controller, Delete, Get, Patch, Post } from '@nestjs/common';
+import {
+  Controller,
+  Delete,
+  Get,
+  HttpCode,
+  HttpStatus,
+  Patch,
+  Post,
+} from '@nestjs/common';
 
 @Controller('projects')
 export class ProjectsController {
+  @HttpCode(HttpStatus.OK)
   @Get()
-  getManyProjects(): string {
+  getManyProjects() {
     return 'This action returns all projects';
   }
 
+  @HttpCode(HttpStatus.OK)
   @Get(':projetcId')
   getOneProject(): string {
     return 'This action returns one project';
   }
-
+  @HttpCode(HttpStatus.CREATED)
   @Post()
   createOneProject(): string {
     return 'This action creates a project';
   }
-
+  @HttpCode(HttpStatus.OK)
   @Patch(':projectId')
   partialUpdateOneProject(): string {
     return 'This action updates a project';
   }
-
+  @HttpCode(HttpStatus.NO_CONTENT)
   @Delete(':projectId')
   removeOneProject(): string {
     return 'This action removes a project';
